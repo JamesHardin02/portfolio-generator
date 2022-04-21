32cebe224c03292d0e0ce2ac65b7f1c31c775a4b
JamesH <jwhusc7@gmail.com>
2022-04-21 11:59:41 -0400

    added capture input function with dynamic output capabilities

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const printProfileData = (profileDataArr) => {
     for (let i = 0; i < profileDataArr.length; i++) {
         console.log(profileDataArr[i]);
     }
+
+    console.log('================');
+
+    // Is the same as this...
+    profileDataArr.forEach((profileItem) => {
+        console.log(profileItem)
+    });
 };
 
 printProfileData(profileDataArgs);
